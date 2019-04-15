33395f28d4c4fda8b1a5e6ee3bb3750c6f06f098
Marcus Leong <mwleong25@gmail.com>
2019-04-15 15:09:32 -0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+/**
+*	@author 
+*	@date 
+*	@file main.cpp
+*	@brief driver for LinkedList demo
+*/
+#include <iostream>
+#include "LinkedListOfInts.h"
+
+int main(int argc, char** argv)
+{
+	//Example of declaring a LinkedListOfInts
+	LinkedListOfInts testableList;
+
+	//You won't do all the tests in main; that's what your Test class will be for
+	TestSuite test;
+	test.runTest();
+
+	std::cout << "Running...\nAnd we're done.\nGoodbye.\n";
+	
+	return (0);
+
+}
+
